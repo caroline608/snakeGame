5f584072519e9d6d761588a1b52111413da36b7d
Evrlasting <cruz.caroline08@gmail.com>
2024-10-18 15:15:01 -0400

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by Caroline Cruz on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
